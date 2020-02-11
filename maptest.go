7c9bfc0cefd8d58547ee200981254b7b7158d769
Simranpal <simranpal.singh@suse.com>
2020-02-11 11:32:57 -0800

    More tests on go maps

--- maptest.go
+++ maptest.go
@@ -11,4 +11,24 @@ func main() {
 	//if value does not exist in map
 	j := mymap["bike"]
 	fmt.Println("value of mymap undefined key \"bike\":", j)
+	//length of map
+	fmt.Println("Length of map: ", len(mymap))
+
+	mymap["trucks"] = 1
+	//iterate over the map
+	for key, value := range mymap {
+		fmt.Printf("\nKey = %s, Value = %d\n", key, value)
+	}
+
+	//initialize map with values
+	map2 := map[string]int{
+		"sum":  3711,
+		"perc": 2138,
+		"gra":  1908,
+		"mes":  912,
+	}
+	fmt.Println("\nLength of map2: ", len(map2))
+
+	val, ifExists := map2["gra"]
+	fmt.Printf("\n\"gra\" val = %d, ifExists = %t \n", val, ifExists)
 }
