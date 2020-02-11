9e86fba41decd5f02443448aa531053bf16c9e17
Simranpal <simranpal.singh@suse.com>
2020-02-11 14:31:43 -0800

    Accessing struct variables

--- structs.go
+++ structs.go
@@ -2,11 +2,20 @@ package main
 
 import "fmt"
 
-type Simran struct {
+type human struct {
 	city string
 	job  string
 }
 
 func main() {
-	fmt.Println(Simran{"Vanc", "SE"})
+	fmt.Println(human{"NYC", "Chef"})
+	//instance of human struct
+	Simran := human{"YVR", "SE"}
+	//access the struct variables with dot operator
+	Simran.city = "Vancouver"
+	fmt.Println("Simran:", Simran)
+	//access the struct via pointer
+	p := &Simran
+	p.city = "Burnaby"
+	fmt.Println("Simran:", Simran)
 }
