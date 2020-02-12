bd82fdd57c6475dba5edb7c1d46dfbf4414b018d
Simranpal <simranpal.singh@suse.com>
2020-02-12 11:40:32 -0800

    Return multiple values from fn

--- funtions.go
+++ funtions.go
@@ -6,6 +6,13 @@ func add(x, y int) int {
 	return x + y
 }
 
+//multiple returns
+func multif(x, y, z string) (string, string, string) {
+	return y, x, z
+}
+
 func main() {
 	fmt.Println(add(42, 13))
+	a, b, c := multif("white", "red", "black")
+	fmt.Println(a, b, c)
 }
